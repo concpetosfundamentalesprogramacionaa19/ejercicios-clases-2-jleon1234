23acbe6dc85b683ebd2f51e7ed6d1bc360bade9d
Juan Pablo <jleon4811@gmail.com>
2019-04-11 16:01:30 -0500

    Subiendo primer commit

---
+++ EjemploClaseDos/src/ejemploclaseuno/EjemploClaseUno.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploclaseuno;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JuanPablo
+ */
+public class EjemploClaseUno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        String apellido;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese sus nombre");
+        nombre = entrada.nextLine();
+        System.out.println("Ingrese sus apellido");
+        apellido = entrada.nextLine();
+        System.out.println("Su nombre es: " + nombre + '\n'+'\n'
+                + "  Su apellido es: " + apellido);
+    }
+    
+}
